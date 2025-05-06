1937b2bb066bbca384e7e472e530030b1fa66b1e
Mayra Vargas <vargasmayra800@gmail.com>
2025-05-06 08:54:54 -0500

    Agregue archivo Usuario.java

---
+++ Documents/NetBeansProjects/ProyectoEntrega7/src/main/java/com/proyectoentrega7/modelo/Usuario.java
@@ -0,0 +1,59 @@
+package com.proyectoentrega7.modelo;
+
+public class Usuario {
+    // Atributos del usuario
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    
+    // Constructor
+    public Usuario(String nombre, String apellido, String email, String password) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+    }
+    
+    // Constructor vacío
+    public Usuario() {
+    }
+    
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+    
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + '}';
+    }
+}
